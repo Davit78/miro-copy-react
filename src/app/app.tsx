4f663feee91f228b7a8655c551c7e4c8e5f588ff
davit-grigoryan1 <36207280+Davit78@users.noreply.github.com>
2025-09-03 12:15:32 +0400

    Add config

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,7 +1,9 @@
 import { Outlet } from "react-router-dom";
 import { AppHeader } from "@/features/header";
+import { CONFIG } from "@/shared/model/config";
 
 export function App() {
+  console.log(CONFIG.API_BASE_URL)
   return (
     <div>
       <AppHeader />
